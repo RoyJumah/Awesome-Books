79c63df8d64ecc2461be5b7f2e7d029f2f64001e
lugard1 <agulugard@gmail.com>
2022-11-29 19:19:29 +0100

    update UI.js and Book.js

---
+++ UI.js
@@ -0,0 +1,27 @@
+import Bookstore from './Bookstore.js';
+
+export default class UI {
+  static addBook(book) {
+    const bookshelf = document.getElementById('bookshelf');
+    const ul = document.createElement('ul');
+    ul.innerHTML = `
+    <li>${book.title}</li>
+    <li>${book.author}</li>
+
+    <button class ="delete" type = "button">Remove</button>
+    `;
+    bookshelf.appendChild(ul);
+  }
+  static removeBook(element) {
+    if (element.classList.contains('delete')) {
+      element.parentElement.remove();
+    }
+  }
+
+  static displayBooks() {
+    const books = Bookstore.getBooks();
+    books.forEach(book => {
+      UI.addBook(book);
+    });
+  }
+}
